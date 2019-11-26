b567cd866d4389dded02039f5a1321570e538c67
Jose <segura2012araya@gmail.com>
2019-11-26 11:00:09 -0600

    Primera Version 0.01

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//=======================================
+//Configuración de Puerto
+//=======================================
+process.env.PORT = process.env.PORT || 3000;
+process.env.Usuario = "Jose Segura"
